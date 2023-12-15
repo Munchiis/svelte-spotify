1b022b68b7cdd2d74325a3cb9bfaa1cbeeeb5b5e
Juan Echevarria <jechevarria00@gmail.com>
2023-12-15 11:04:29 -0800

    added logout functionality, endpoint, and component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	import type { LayoutData } from './$types';
-    import 'modern-normalize/modern-normalize.css'
-    import '../styles/main.scss';
-    
-    export let data: LayoutData;
+	import 'modern-normalize/modern-normalize.css';
+	import '../styles/main.scss';
+	import { LogoutButton } from '$lib/components/index';
+
+	export let data: LayoutData;
+
+	$: user = data.user;
 </script>
 
-{data.user?.display_name}
+{#if user}
+	<p>Hello {user.display_name}</p>
+	<LogoutButton/>
+{/if}
 
 <slot />
