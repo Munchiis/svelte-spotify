a918ad0421989f04a4c72c5010cf1f824dafe613
Juan Echevarria <jechevarria00@gmail.com>
2023-12-15 12:09:35 -0800

    added button styles

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	export let element: 'button' | 'a';
+	export let variant: 'solid' | 'outline' | 'red' = 'solid';
+	export let className: string = '';
+</script>
+
+<svelte:element
+	this={element}
+	class="button button-{variant} {className}"
+	on:click
+	{...$$restProps}
+>
+	<slot />
+</svelte:element>
+
+<style lang="scss">
+	.button {
+		display: inline-block;
+		border: none;
+		font-weight: 600;
+		font-size: functions.toRem(14);
+		border-radius: functions.toRem(20);
+		cursor: pointer;
+		padding: functions.toRem(7) functions.toRem(15);
+		text-decoration: none;
+		&.button-solid {
+			background-color: var(--accent-color);
+			color: #000;
+			border: functions.toRem(2) solid var(--accent-color);
+		}
+		&.button-outline {
+			background: none;
+			color: var(--text-color);
+			border: functions.toRem(2) solid;
+		}
+		&.button-red {
+			background-color: var(--error);
+			color: #fff;
+			border: functions.toRem(2) solid var(--error);
+		}
+		&:disabled {
+			opacity: 0.6;
+			cursor: not-allowed;
+		}
+		&:hover {
+			&.button-solid,
+			&.button-red {
+				background-image: linear-gradient(rgba(0, 0, 0, 0.1) 0 0);
+                border-image: linear-gradient(rgba(0, 0, 0, 0.1) 0 0);
+
+			}
+			&.button-outline {
+				background-image: linear-gradient(rgba(255, 255, 255, 0.1) 0 0);
+			}
+		}
+		&:active {
+			&.button-solid,
+			&.button-red {
+				background-image: linear-gradient(rgba(255, 255, 255, 0.1) 0 0);
+                border-image: linear-gradient(rgba(255, 255, 255, 0.1) 0 0);
+			}
+            &.button-outline {
+				background-image: linear-gradient(rgba(255, 255, 255, 0.2) 0 0);
+			}
+		}
+	}
+</style>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,7 @@
-<a href="/api/auth/login">Login</a>
+<script lang="ts">
+    import {Button} from "$lib/components/index"
+</script>
+
+<Button element='a' variant='outline'>Login</Button>
+<Button element='button'>Login</Button>
+<Button element='button' variant='red' disabled>Login</Button>
